044a0fdbb16e07578f2dfe3a5d98dabeb1b26743
coffeemk2 <yana.sd.iml@gmail.com>
2019-04-23 01:42:40 +0900

    enable hot reload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import clearConsole from "../utils/clearConsole";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World"));
+app.get("/app", (req, res) => {
+  console.log("hit /app");
+  res.send("Hello World2");
+});
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => {
+  clearConsole();
+  console.log("Example app listening on port 3000!");
+});
---
+++ utils/clearConsole.js
@@ -0,0 +1,5 @@
+function clearConsole() {
+  console.log("\x1B[2J\x1B[3J\x1B[H");
+}
+
+module.exports = clearConsole;
